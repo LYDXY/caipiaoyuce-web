c54f6f6180ec936c2d96c0c1734fbde231e8f844
cswzxly <1542319159@qq.com>
2015-12-13 14:58:03 +0800

    正式搭建项目使用 Struts2+spring4+mybatis

---
+++ Zwebcaipiaoyuce/src/com/web/action/SixOneAction.java
@@ -0,0 +1,36 @@
+package com.web.action;
+
+import java.util.List;
+
+import javax.annotation.Resource;
+
+import com.web.pojo.SixOne;
+import com.web.services.SixOneServices;
+
+/**
+ * 
+ * @author 林尧
+ * 
+ *         2015年12月12日
+ */
+public class SixOneAction extends BaseAction {
+    
+	private String TAG="SixOneAction";
+	private static final long serialVersionUID = 1L;
+
+	@Resource(name = "SixOneServices")//接口的实现
+	private SixOneServices SixOneServices; // 服务层的接口 
+    
+	/**
+	 * 查询所有的期数 数据
+	 * @return
+	 */
+	public String getSixOnes(){
+		logger.info(TAG+"---"+"getSixOnes");
+		
+		List<SixOne> all_sixones1=SixOneServices.way0();
+		logger.info(TAG+"---该表格的数据总数"+all_sixones1.size());
+		return "GETSIXONES_SUCCESS";
+	}
+
+}
