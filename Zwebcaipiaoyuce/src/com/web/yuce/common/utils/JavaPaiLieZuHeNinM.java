17aa8e284a7f7f2345c7802146c74980af5706b9
林尧 <1542319159@qq.com>
2015-12-30 15:41:17 +0800

    s

--- Zwebcaipiaoyuce/src/com/web/yuce/common/utils/JavaPaiLieZuHeNinM.java
+++ Zwebcaipiaoyuce/src/com/web/yuce/common/utils/JavaPaiLieZuHeNinM.java
@@ -10,23 +10,38 @@ import java.util.List;
  */
 public class JavaPaiLieZuHeNinM {
 
+	
+	//准备数据
 	private static String[] numbers = new String[] { "11", "12", "13", "15" };
-
+    
+	//记录最终有多少种组合
 	private static int total;
-
+    
+	//设置1组 有几个数字
 	private static int m = 3;
 
+	/**
+	 * 递归算法
+	 * @param s
+	 * @param templist
+	 * @param m
+	 */
 	private void plzh(String s, List<Integer> templist, int m) {
 		if (m == 0) {
-
 			System.out.println(s);
 			total++;
 			return;
 		}
+		
+		
 		List<Integer> iL2;
+		
+		//4 个数字  4次
 		for (int i = 0; i < numbers.length; i++) {
-
+            
+			//每一次创建一个新的数组集合
 			iL2 = new ArrayList<Integer>();
+			
 			iL2.addAll(templist);
 			if (!templist.contains(i)) {
 				String str = s + "----" + numbers[i];
